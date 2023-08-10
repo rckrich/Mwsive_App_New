c3690f101085c691c5aba7e088796372a2201d9d
José Javier Serrano Solís <javierserranosolis@rckgames.com>
2023-08-10 09:54:56 -0600

    Primer adelanto de rankeo

--- Assets/Scripts/ViewModels/ViewModels/RankingViewModel.cs
+++ Assets/Scripts/ViewModels/ViewModels/RankingViewModel.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RankingViewModel : ViewModel
+public class RankingViewModel : ScrollViewModel
 {
    public GameObject selectTime;
-  
+
+    public override void Initialize(params object[] list)
+    {
+        StartSearch();
+        MwsiveConnectionManager.instance.GetRanking("", Callback_GetRanking);
+    }
+
+    private void Callback_GetRanking(object[] _list)
+    {
+        EndSearch();
+    }
+
     public void OnClick_SelectTime()
     {
         selectTime.SetActive(true);
