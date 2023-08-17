8d234e2ed1b4e9a904a1aab4f5259ad6ac4c9501
Dylan Ocampo <dylanomar@live.com.mx>
2023-08-17 09:12:30 -0600

    DeepLinking start

--- Assets/Scripts/Other/NativeShare.cs
+++ Assets/Scripts/Other/NativeShare.cs
@@ -22,6 +22,26 @@ public class NativeShare : MonoBehaviour
     {
         
     }
+    private void OnEnable ()
+    {
+        DeepLinkServices.OnCustomSchemeUrlOpen     += OnCustomSchemeUrlOpen;
+        DeepLinkServices.OnUniversalLinkOpen       += OnUniversalLinkOpen;
+    }
+
+    private void OnDisable ()
+    {
+        DeepLinkServices.OnCustomSchemeUrlOpen    -= OnCustomSchemeUrlOpen;
+        DeepLinkServices.OnUniversalLinkOpen      -= OnUniversalLinkOpen;
+    }
+    private void OnCustomSchemeUrlOpen (DeepLinkServicesDynamicLinkOpenResult result)
+    {
+        Debug.Log("Handle deep link : " + result.Url);
+    }
+
+    private void OnUniversalLinkOpen (DeepLinkServicesDynamicLinkOpenResult result)
+    {
+        Debug.Log("Handle deep link : " + result.Url);
+    }
 
     public void OnClickShareMwsiveSong(){
         
