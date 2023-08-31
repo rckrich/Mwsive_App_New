27f494f13f51691685ce57b7a3209167f321fc4a
RodrigoCuevasCa <108311383+RodrigoCuevasCa@users.noreply.github.com>
2023-08-31 11:30:43 -0600

    fix

--- Assets/Scripts/Other/AddSongOptions.cs
+++ Assets/Scripts/Other/AddSongOptions.cs
@@ -16,13 +16,6 @@ public class AddSongOptions : ViewModel
     public TrackViewModel trackViewModel;
     public string url;
     public string uri;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        
-    }
-
 
     public void OnClick_AddItemsToPlaylist()
     {
