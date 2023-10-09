65625a429a53ff411bd8d79723785df0fd6d66cb
José Javier Serrano Solís <javierserranosolis@rckgames.com>
2023-10-09 13:37:29 -0600

    Ola Animation Fixes

--- Assets/Scripts/Tests/AniTest.cs
+++ Assets/Scripts/Tests/AniTest.cs
@@ -6,6 +6,119 @@ using UnityEngine.UIElements;
 
 public class AniTest : MonoBehaviour
 {
+    public Transform restPosition, left;
+    private Vector2 initialValue;
+
+    public RectTransform SecondWaveMask, restPosition2;
+
+    public bool Follow;
+    Tweener ani;
+    float starttween;
+    
+
+    private void Update()
+    {
+        if (Follow)
+        {
+            Vector3 Pos = restPosition.transform.position;
+            Debug.Log(gameObject.GetComponent<RectTransform>().rect.height);
+            //Pos.y = Pos.y - gameObject.GetComponent<RectTransform>().rect.height;
+
+            //gameObject.transform.position = Pos;
+        }
+        
+    }
+
+    void Start()
+    {
+        if (!Follow)
+        {
+            initialValue = gameObject.GetComponent<RectTransform>().offsetMax;
+        }
+        else
+        {
+            Debug.Log(gameObject.GetComponent<RectTransform>().rect.width);
+
+            float val = gameObject.GetComponent<RectTransform>().rect.width;
+
+
+            SecondWaveMask.offsetMin = new Vector2(val, SecondWaveMask.offsetMin.y);
+            SecondWaveMask.offsetMax = new Vector2(val, SecondWaveMask.offsetMax.y);
+            restPosition2.offsetMin = new Vector2(-val, SecondWaveMask.offsetMin.y);
+            restPosition2.offsetMax = new Vector2(-val, SecondWaveMask.offsetMax.y);
+
+        }
+                
+        StartAnimation();
+    }
+
+
+    public void StartAnimation()
+    {
+        if (Follow)
+        {
+            if(ani == null)
+            {
+                FromCenterToLeft();
+            }
+            else
+            {
+                ani.Restart();
+            }
+        }
+        else
+        {
+            if (ani == null)
+            {
+                gameObject.GetComponent<RectTransform>().offsetMax = initialValue;
+                starttween = gameObject.GetComponent<RectTransform>().offsetMax.y;
+                ColorUp();
+
+            }
+            else
+            {
+                ani.Restart();
+            }
+        }
+
+    }
+
+    private void FromCenterToLeft()
+    {
+        ani = gameObject.transform.DOMoveX(left.position.x, 3).SetLoops(-1).SetEase(Ease.Linear);
+    }
+
+    private void ColorUp()
+    {
+
+        float twenable = starttween;
+        
+        ani = DOTween.To(() => twenable, x => twenable = x, 0f, 30f);
+        ani.OnUpdate(() => {
+            gameObject.GetComponent<RectTransform>().offsetMax = new Vector2(gameObject.GetComponent<RectTransform>().offsetMax.x, twenable);
+            if (twenable > -2)
+            {
+
+                //waveMask.transform.DOMoveY(leftRestPosition.transform.position.y, 2f).OnComplete(() => {
+
+                //}).SetEase(Ease.Linear);
+            }
+        });
+
+        ani.SetEase(Ease.InSine);
+        ani.OnComplete(() =>
+        {
+
+            ani.Complete();
+
+        });
+        ani.SetAutoKill(false);
+
+
+    }
+
+
+    /*
     public GameObject Top1, Position3;
     public GameObject Color;
     private Vector3 Left;
@@ -52,4 +165,5 @@ public class AniTest : MonoBehaviour
     {
         
     }
+    */
 }
