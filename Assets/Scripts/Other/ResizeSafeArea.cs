7ec84ee10a7be8156eebf6f83225fe85a223fa0d
José Javier Serrano Solís <javierserranosolis@rckgames.com>
2023-10-19 14:35:44 -0600

    Possible hot fix for status bar

---
+++ Assets/Scripts/Other/ResizeSafeArea.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeSafeArea : MonoBehaviour
+{
+    RectTransform rectTransform;
+    Rect safeArea;
+    Vector2 minAnchor;
+    Vector2 maxAnchor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        safeArea = Screen.safeArea;
+        minAnchor = safeArea.position;
+        maxAnchor = minAnchor + safeArea.size;
+
+        minAnchor.x /= Screen.width;
+        minAnchor.y /= Screen.height;
+        maxAnchor.x /= Screen.width;
+        maxAnchor.y /= Screen.height;
+
+        rectTransform.anchorMin = minAnchor;
+        rectTransform.anchorMax = maxAnchor;
+    }
+
+}
