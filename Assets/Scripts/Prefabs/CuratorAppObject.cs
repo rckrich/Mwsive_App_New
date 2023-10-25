60dade41fad240a079931c013896d0175169987d
RodrigoCuevasCa <108311383+RodrigoCuevasCa@users.noreply.github.com>
2023-10-25 14:38:53 -0600

    fix usuarios que votaron

--- Assets/Scripts/Prefabs/CuratorAppObject.cs
+++ Assets/Scripts/Prefabs/CuratorAppObject.cs
@@ -15,33 +15,46 @@ public class CuratorAppObject : AppObject
 
     public override void Initialize(params object[] list) {
         mwsiveUser = (MwsiveUser)list[0];
-
-        mwsiveID = mwsiveUser.id.ToString();
-        spotifyID = mwsiveUser.platform_id.ToString();
-
-        if (mwsiveUser.display_name.Length > 27)
+        if (mwsiveUser != null)
         {
-            string _text2 = "";
-            for (int k = 0; k < 27; k++)
+            mwsiveID = mwsiveUser.id.ToString();
+            spotifyID = mwsiveUser.platform_id.ToString();
+
+            if (mwsiveUser.display_name.Length > 27)
             {
+                string _text2 = "";
+                for (int k = 0; k < 27; k++)
+                {
 
-                _text2 = _text2 + mwsiveUser.display_name[k];
+                    _text2 = _text2 + mwsiveUser.display_name[k];
 
+                }
+                _text2 = _text2 + "...";
+                displayName.text = _text2;
             }
-            _text2 = _text2 + "...";
-            displayName.text = _text2;
+            else
+            {
+                displayName.text = mwsiveUser.display_name;
+            }
+            if (mwsiveUser.image_url != null)
+                ImageManager.instance.GetImage(mwsiveUser.image_url, profilePicture, (RectTransform)this.transform, "PROFILEIMAGE");
         }
         else
         {
-            displayName.text = mwsiveUser.display_name;
+            mwsiveID = "";
+            displayName.text = "Usuario no disponible";
         }
-        if (mwsiveUser.image_url != null)
-            ImageManager.instance.GetImage(mwsiveUser.image_url, profilePicture, (RectTransform)this.transform, "PROFILEIMAGE");
     }
 
     public void OnClick_CuratorAppObject(){
-
-        NewScreenManager.instance.ChangeToSpawnedView("profile");
-        NewScreenManager.instance.GetCurrentView().Initialize(spotifyID, mwsiveID);
+        if (mwsiveID.Equals(""))
+        {
+            UIMessage.instance.UIMessageInstanciate("Este usuario no está disponible");
+        }
+        else
+        {
+            NewScreenManager.instance.ChangeToSpawnedView("profile");
+            NewScreenManager.instance.GetCurrentView().Initialize(spotifyID, mwsiveID);
+        }
     }
 }
